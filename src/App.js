814547c4efd6191b7eb72d130973d4eadbd2d972
Kazymbaev Aidar <kazymbaevaidar@gmail.com>
2021-11-25 22:26:51 +0600

    verstkaReact

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import About from "./components/About/About";
+import BlockOfWatches from "./components/BlockOfWatches/BlockOfWatches";
+import BlockOne from "./components/BlockOne/BlockOne";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import OurBrands from "./components/OurBrands/OurBrands";
+import WatchesBlock from "./components/WatchesBlock/WatchesBlock";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header />
+        <BlockOne />
+        <WatchesBlock />
+        <BlockOfWatches />
+        <OurBrands />
+        <About />
+        <Footer />
     </div>
   );
 }
